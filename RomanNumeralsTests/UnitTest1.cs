e4c12b751fbc884f8d876b6eb247c243745d470e
Haroldas Varanauskas <hpharis258@gmail.com>
2024-09-15 18:29:20 +0100

    Roman Numerals

--- RomanNumeralsTests/UnitTest1.cs
+++ RomanNumeralsTests/UnitTest1.cs
@@ -4,22 +4,51 @@ namespace RomanNumeralsTests;
 public class Tests
 {
     
-    [Test]
-    public void Test1()
+    [TestCase(1, "I")]
+    [TestCase(2, "II")]
+    [TestCase(3, "III")]
+    [TestCase(4, "IV")]
+    [TestCase(5, "V")]
+    [TestCase(9, "IX")]
+    [TestCase(10, "X")]
+    [TestCase(14, "XIV")]
+    public void ParseTest(int expected, string roman)
     {
-        Assert.AreEqual(1, Roman.Parse("I"));
-        Assert.AreEqual(5, Roman.Parse("V"));
+        Assert.AreEqual(expected, Roman.Parse(roman));
     }
 }
 
 public class Roman
 {
+    private static readonly Dictionary<char, int> map = new Dictionary<char, int>()
+    {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10 },
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
     public static int Parse(string roman)
     {
-        if (roman == "V")
+        int output = 0;
+        for (int i = 0; i < roman.Length; i++)
         {
-            return 5;
+            if (i + 1 < roman.Length && IsSubtractive(roman[i], roman[i+1]))
+            {
+                output -= map[roman[i]];
+            }
+            else
+            {
+                output += map[roman[i]];
+            }
         }
-        return 1;
+        return output;
+    }
+
+    public static bool IsSubtractive(char a, char b)
+    {
+        return  map[a] < map[b];
     }
 }
